6cd0e671a9bc55f7d41cfbbf7ed5d32ea0a77e31
rascarlo <rascarlo@gmail.com>
2012-11-23 15:59:56 +0100

    Volumerocker music control
    Signed-off-by: Paul Beeler <sparksco@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2311,11 +2311,17 @@ public final class Settings {
          */
         public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
 
-        /** Volume Adjust Sounds Enable, This is the noise made when using volume hard buttons
+       /** Volume Adjust Sounds Enable, This is the noise made when using volume hard buttons
         * Defaults to 1 - sounds enabled
         * @hide
         */
         public static final String VOLUME_ADJUST_SOUNDS_ENABLED = "volume_adjust_sounds_enabled";
+
+        /**
+         * Whether or not volume button music controls should be enabled to seek media tracks
+         * @hide
+         */
+        public static final String VOLBTN_MUSIC_CONTROLS = "volbtn_music_controls";
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
