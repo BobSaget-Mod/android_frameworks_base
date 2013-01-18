43f543c1b76faf663892afc0898aa3da69c6ee4b
Danesh Mondegarian <daneshm90@gmail.com>
2013-01-18 16:46:04 -0500

    SystemUI : QuickSettings Cleanup
    Introduce new class to reference from settings and SystemUI to avoid
    code duplication.
    
    Differentiate between configuration change vs tile layout modification.
    
    Patchset 2  - Remove whitespace
    Patchset 3  - Remove unused ids
    Patchset 4  - Better organize the classes
    Patchset 5  - Rebase
    Patchset 6  - Introduce updateResources()
    Patchset 8  - Update tiles to use updateResources()
    Patchset 9  - Expand imports
    Patchset 10 - Updated all tiles to support updateResources()
                  Added dinamic tiles to mQuickSettingsTiles
                  Optimized and standarized of calls to updateQuickSettings() from tiles
                     onPostCreate() -> updateTile() -> super.onPostCreate() -> updateQuickSettings()
                     updateResources() -> updateTile() -> super.updateResources() -> updateQuickSettings()
                  Rebased
    Patchset 11 - Remove unnecessary calls to update Quick Settings
    
    Signed-off-by: Paul Beeler <sparksco@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -47,6 +47,7 @@ import android.graphics.PorterDuff;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.inputmethodservice.InputMethodService;
+import android.net.Uri;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.IPowerManager;
@@ -703,7 +704,7 @@ public class PhoneStatusBar extends BaseStatusBar {
                 }
                 mQS.setService(this);
                 mQS.setBar(mStatusBarView);
-                mQS.updateResources();
+                mQS.setupQuickSettings();
 
                 // Start observing for changes
                 mTilesChangedObserver = new TilesChangedObserver(mHandler);
@@ -2932,8 +2933,13 @@ public class PhoneStatusBar extends BaseStatusBar {
 
         @Override
         public void onChange(boolean selfChange) {
+            onChange(selfChange, null);
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
             if (mSettingsContainer != null) {
-                mQS.updateResources();
+                mQS.setupQuickSettings();
             }
         }
 
