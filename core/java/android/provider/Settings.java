155e3413f8c44fb35888450065b05b3f20ad9af6
Paul Beeler <sparksco@gmail.com>
2013-02-14 22:44:30 -0600

    framework: Battery bar (1/2)
    * Patch inspired by MIUI and originally ported by David Morgan / @Spitemare
    * as a extra feature for custom @CyanogenMod 7 ROMs.  Patch has been adapted
    * to work better on @SaberMod.  Much of the work was done by Daniel Nilsson / @syaoran12
    * Patch is nearly identical in nature to how Dan has writen it, with a few
    * minor changes in removing the option to have centered bar, which doesn't
    * seem to work very well on some devices like the Nexus 7 (grouper).  Also
    * does not check for devices with no navigation bar as @SaberMod only supports
    * nexus devices that all have navigation bars.
    
    Signed-off-by: Paul Beeler <sparksco@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2576,6 +2576,27 @@ public final class Settings {
        public static final String STATUS_BAR_BRIGHTNESS_CONTROL = "status_bar_brightness_control";
 
         /**
+         * Whether to show the battery bar
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR = "statusbar_battery_bar";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_COLOR = "statusbar_battery_bar_color";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_THICKNESS = "statusbar_battery_bar_thickness";
+
+        /**
+         * @hide
+         */
+        public static final String STATUSBAR_BATTERY_BAR_ANIMATE = "statusbar_battery_bar_animate";
+
+        /**
          * Custom navring actions
          *
          * @hide
