98b2dbac9953f36c173bd16c6978bb6f14d08680
Danny Baumann <dannybaumann@web.de>
2013-06-20 08:52:48 +0200

    Derp fix.
    Signed-off-by: Paul Beeler <sparksco@gmail.com>

--- packages/SystemUI/src/com/android/systemui/SearchPanelView.java
+++ packages/SystemUI/src/com/android/systemui/SearchPanelView.java
@@ -44,8 +44,7 @@ import android.view.ViewTreeObserver;
 import android.view.ViewTreeObserver.OnPreDrawListener;
 import android.widget.FrameLayout;
 
-import static com.android.internal.util.saber.NavigationRingConstants.*;
-
+import com.android.internal.util.saber.NavigationRingConstants;
 import com.android.internal.util.saber.NavigationRingHelpers;
 import com.android.internal.widget.multiwaveview.GlowPadView;
 import com.android.internal.widget.multiwaveview.GlowPadView.OnTriggerListener;
