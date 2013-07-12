ebb92edd33baf9e1f72b9a50b82bd289ea9f3e11
Paul Beeler <sparksco@gmail.com>
2013-07-12 14:41:29 -0600

    Revert "Add lockscreen rotation as an optional rotation setting (1/2)"
    This reverts commit f83d37191ea244da3ef717ebc9d4996d19bab47e.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2956,19 +2956,6 @@ public final class Settings {
         public static final String ALLOW_ALL_ROTATIONS = "allow_all_rotations";
 
         /**
-         * Control the type of rotation which can be performed using the accelerometer
-         * if ACCELEROMETER_ROTATION is enabled.
-         * Value is a bitwise combination of
-         * 1 = 0 degrees (portrait)
-         * 2 = 90 degrees (left)
-         * 4 = 180 degrees (inverted portrait)
-         * 8 = 270 degrees (right)
-         * Setting to 0 is effectively orientation lock
-         * @hide
-         */
-        public static final String ACCELEROMETER_ROTATION_ANGLES = "accelerometer_rotation_angles";
-
-        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -3052,8 +3039,7 @@ public final class Settings {
             POWER_MENU_SILENT_ENABLED,
             CHARGING_LED_ENABLED,
             LOW_BATTERY_LED_PULSE_ENABLED,
-            ALLOW_ALL_ROTATIONS,
-            LOCKSCREEN_ROTATION
+            ALLOW_ALL_ROTATIONS
         };
 
         // Settings moved to Settings.Secure
